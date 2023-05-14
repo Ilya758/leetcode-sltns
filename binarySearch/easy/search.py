9f547722bdb1bfc19263a46992c1bbb1a9ada514
Illia Skaryna <subscriber6436@gmail.com>
2023-05-14 13:04:23 +0300

    feat: solved binarySearch-easy-search

---
+++ binarySearch/easy/search.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/binary-search/
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        mid = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+
+print(Solution().search(nums=[-1, 0, 3, 5, 9, 12], target=9))  # 4
