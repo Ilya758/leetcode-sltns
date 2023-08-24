502ae422da5c1513b117771ea9213d4f11bdd562
Illia Skaryna <subscriber6436@gmail.com>
2023-08-24 22:57:46 +0300

    feat: solved ll-easy-hasCycle

---
+++ linkedLists/easy/hasCycle.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/linked-list-cycle/description/
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from typing import Optional
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
