516d610202033e10d33f022a33a862d51280e1f6
Illia Skaryna <subscriber6436@gmail.com>
2023-09-30 18:52:34 +0300

    feat: solved greedy-medium-removeDuplicateLetters

---
+++ greedy/medium/removeDuplicateLetters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        last_occurrence = {char: i for i, char in enumerate(s)}
+        
+        for i, char in enumerate(s):
+            if char in stack:
+                continue
+            
+            while stack and char < stack[-1] and i < last_occurrence[stack[-1]]:
+                stack.pop()
+                
+            stack.append(char)
+            
+        return ''.join(stack)
+
+print(Solution().removeDuplicateLetters(s = "bcabc")) # abc
