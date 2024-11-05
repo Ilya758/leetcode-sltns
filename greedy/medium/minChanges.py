d2b1324076596600fc72a33c8bc867ecce685f7e
Illia Skaryna <subscriber6436@gmail.com>
2024-11-05 18:43:59 +0300

    feat: solved greedy-medium-minChanges

---
+++ greedy/medium/minChanges.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        ans = 0
+
+        for i in range(0, len(s), 2):
+            if s[i] != s[i + 1]:
+                ans += 1
+
+        return ans
+    
+print(Solution().minChanges("11000111")) # 1
